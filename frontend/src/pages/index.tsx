0dddb28932888b08e21c79a98edb8a88a3115475
Juan_Victor <estudantejuan0@gmail.com>
2023-01-20 10:57:25 -0300

    Criando document no frontend

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { Inter } from '@next/font/google'
+import Head from 'next/head';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>SujeitoPizza - Faça seu login</title>
+    </Head>
     <div>
-      <h1>Sujeito Pizza</h1>
+      <h1>Sujeito Pizzaria</h1>
     </div>
+    </>
   )
 }
