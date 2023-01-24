6c2c209dd86f7aa166c6242171280c661ac3f4e9
Juan_Victor <estudantejuan0@gmail.com>
2023-01-24 11:33:38 -0300

    Rotas privadas - Autenticação para usuário logado

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,7 +1,13 @@
-
+import { canSSRAuth } from '../../utils/canSSRAuth';
 
 export default function Dashboard(){
     return(
         <h1>Bem vindo ao painel</h1>
     )
 }
+
+export const getServerSideProps = canSSRAuth(async () => {
+    return {
+        props: {}
+    }
+})
