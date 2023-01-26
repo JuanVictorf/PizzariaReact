f9633f998ba276cd2047f39f26d491f8df316561
Juan_Victor <estudantejuan0@gmail.com>
2023-01-26 11:48:25 -0300

    Configurando modal para aparecer o detalhes pedid

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -8,6 +8,10 @@ import { FiRefreshCcw } from 'react-icons/fi';
 
 import { setupAPIClient } from '../../services/api';
 
+import { ModalOrder } from '../../components/ModalOrder';
+
+import Modal from 'react-modal';
+
 type OrderProps = {
     id: string;
     table: string | number;
@@ -20,16 +24,52 @@ interface HomeProps{
     orders: OrderProps[];
 }
 
+export type OrderItemProps = {
+    id: string;
+    amount: number;
+    order_id: string;
+    product_id: string;
+    product: {
+        id: string;
+        name: string;
+        description: string;
+        price: string;
+        banner: string;
+    }
+    order: {
+        id: string;
+        table: string | number;
+        status: boolean;
+        name: string | null;
+    }
+}
+
 export default function Dashboard({ orders }: HomeProps){
     
     const [orderList, setOrderList] = useState(orders || []);
 
+    const [modalItem, setModalItem] = useState<OrderItemProps[]>();
+    const [modalVisible, setModalVisible] = useState(false);
 
-    function handleOpenModalView(id: string){
-        alert("Id clicado: " + id);
+    
+    function handleCloseModal(){
+        setModalVisible(false);
+    }
+    
+    async function handleOpenModalView(id: string){
+        const apiClient = setupAPIClient();
+
+        const response = await apiClient.get('/order/detail', {
+            params: {
+                order_id: id,
+            }
+        });
+
+        setModalItem(response.data);
+        setModalVisible(true);
     }
 
-
+    Modal.setAppElement('#__next');
 
     return(
         <>
@@ -65,6 +105,10 @@ export default function Dashboard({ orders }: HomeProps){
             
             </main>
 
+
+            {modalVisible && (
+                <ModalOrder/>
+            )}            
 
         </div>
         </>
